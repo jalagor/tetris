04ce5203107d0362c0c167d29f09cc2ca22dc2c8
jalagor <johnrossrogla@gmail.com>
2020-02-07 19:39:23 -0700

    added components and NavBar

---
+++ frontend/src/components/Board.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Board = ()=> {
+    return (
+        <div className="board-container">
+            <div className="board">
+
+            </div>
+        </div>
+    )
+}
